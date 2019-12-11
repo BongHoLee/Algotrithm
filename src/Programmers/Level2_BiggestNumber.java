9361570032b3ba420f27f9e64ed547bb596a6c3b
leebongho <bh9204@naver.com>
2019-12-12 05:27:27 +0900

    가장 큰 수 풀이 완료.
    -> subList 구현 제거, index로 서치하게끔 수정 (subList를 만들기 위한 반복문 제거)
    
    -> 가장 첫 자리가 가장 큰 수 일 때 생기는 문제 해결 (cutoff만큼 숫자 제거되면 재귀 탈출 조건 삽입)

--- src/Programmers/Level2_BiggestNumber.java
+++ src/Programmers/Level2_BiggestNumber.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public class Level2_BiggestNumber {
     public static void main(String[] args) {
-        String number = "4177252841";
-        int k = 4;
+        String number = "10000";
+        int k = 2;
 
         //775841
         // 4,1,2,2 제거
@@ -22,31 +22,31 @@ public class Level2_BiggestNumber {
 
         int[] numArr = new int[number.length()];
         char[] cs = number.toCharArray();
-        
+        List<Integer> resultList = new ArrayList<>();
 
         for (int i=0; i<number.length(); i++) {
             numArr[i] = ((int)cs[i] - 48);
         }
 
-
-        searchNum(numArr,answer, k);
+        int makeSize = numArr.length - k;
+        searchNum(numArr,resultList, k, 0, makeSize);
+        for (Integer integer : resultList) answer.append(integer);
         return answer.toString();
     }
 
-    public static void searchNum(int[] numArr, StringBuilder answer, int cutoff) {
-        if (cutoff <= 0) {
-
-            for (int i=0; i<numArr.length; i++)
-                answer.append(numArr[i]);
+    public static void searchNum(int[] numArr, List<Integer> resultList, int cutoff, int curIdx, int makeSize) {
+        if (cutoff <=0) {
+            for (int i=curIdx; i<numArr.length; i++)
+                resultList.add(numArr[i]);
             return;
-        } else if (numArr.length - cutoff <= 0) {
+        } else if (resultList.size() >= makeSize ) {
             return;
         } else {
 
-            int max = 0;
-            int idx = 0;
+            int max = -1;
+            int idx = curIdx;
 
-            for (int i=0; i<=cutoff; i++) {
+            for (int i=curIdx; i<=cutoff+curIdx; i++) {
                 if (numArr[i] == 9) {
                     max = 9;
                     idx = i;
@@ -59,17 +59,12 @@ public class Level2_BiggestNumber {
             }
 
 
-            answer.append(numArr[idx]);
-
-            int[] nextArr = new int[numArr.length - idx-1];
-            cutoff = cutoff - idx;
-            for (int i=0; i<nextArr.length; i++) {
-                nextArr[i] = numArr[idx+1];
-                idx +=1;
-            }
+            resultList.add(numArr[idx]);
+            cutoff = cutoff - (idx - curIdx);
+            idx += 1;
 
             //nums = nums.subList(idx+1, nums.size());
-            searchNum(nextArr, answer, cutoff);
+            searchNum(numArr, resultList, cutoff, idx, makeSize);
         }
 
     }

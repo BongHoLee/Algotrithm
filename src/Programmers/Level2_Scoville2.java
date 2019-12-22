270f94411dbdf3ec54ed2dd05e49fc50b0643d93
leebongho <bh9204@naver.com>
2019-12-23 06:06:13 +0900

    Scoville 1 문제는 재귀로, Scoviile 2 문제는 단순 while 반복문으로 해결함.
    효율성 테스트에서 재귀가 통과하지 못해서 검색중에 단순 반복으로 풀어낸 문제를 확인 (로직은 똑같음)
    
    시간복잡도가 더 크지는 않지만 실제 재귀가 루프보다 효율성면에서 더 떨어지는듯 함
    
    그래도 재귀로 풀어봤다는 점에 의의를 둠

---
+++ src/Programmers/Level2_Scoville2.java
@@ -0,0 +1,51 @@
+package Programmers;
+
+import java.util.PriorityQueue;
+
+/*
+ *  힙을 이용한 알고리즘 문제풀이.
+ *  1. 힙은 완전이진트리로 구현된 자료구조로써 우선순위 큐를 위해 사용되어진다.
+ *  2. 힙은 느슨한 정렬상태를 유지한다 (부모노드 >= 자식노드)
+ *  3. 왼쪽 자식노드 인덱스 = 부모노드인덱스 * 2
+ *  4. 오른쪽 자식노드 인덱스 = (부모노드인덱스 * 2) + 1
+ *
+ *  우선적으로 우선순위 큐를 이용하여 문제를 풀어보자.
+ * */
+public class Level2_Scoville2 {
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+
+        System.out.println(solution(scoville, K));
+    }
+
+    public static int solution(int[] scovilles, int K) {
+        int mixCount = 0;
+        int answer = -1;
+        PriorityQueue<Integer> priorityScoville = new PriorityQueue<>();
+        int firstScoville = 0;
+        int secondScoville = 0;
+
+        for (int scovill : scovilles)
+            priorityScoville.offer(scovill);
+
+        while (priorityScoville.size() > 1) {
+            if (priorityScoville.peek() >= K) {
+                answer = mixCount;
+                break;
+            }
+
+            firstScoville = priorityScoville.poll();
+            secondScoville = priorityScoville.poll();
+            priorityScoville.offer(firstScoville + (secondScoville*2));
+            mixCount += 1;
+        }
+
+        if (priorityScoville.peek() >= K)
+            answer = mixCount;
+        return answer;
+    }
+
+
+
+}

d56822e888ca87b7ec8f71eb3ce6f2f460366bd0
leebongho <bh9204@naver.com>
2020-01-13 07:25:53 +0900

    프로그래머스 레벨2 라면공장 문제 풀이중. 리스트 얕은복사에서 막힘

--- src/TestCode.java
+++ src/TestCode.java
@@ -5,19 +5,14 @@ import java.util.List;
 
 public class TestCode {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
 
-        List<Integer> tmp1 = new ArrayList<>();
-        List<Integer> tmp2 = new ArrayList<>();
-        tmp1.add(1);
-        tmp1.add(2);
-        tmp1.add(3);
-
-        tmp2.addAll(tmp1.subList(0, 2));
-        tmp2.sort(Collections.reverseOrder());
-
-
-        System.out.println(tmp2);
-        System.out.println(tmp1);
+        System.out.println(list.subList(5, list.size()));
 
     }
 

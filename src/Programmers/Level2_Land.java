4906b2397f13f76f653d8dec350f416fd5292b37
leebongho <bh9204@naver.com>
2020-02-07 07:21:54 +0900

    프로그래머스 레벨2 재귀 + 메모이제이션 이용하여 문제 풀이 완료.
    스스로 풀었다는 자신감이 뿜뿜

--- src/Programmers/Level2_Land.java
+++ src/Programmers/Level2_Land.java
@@ -13,28 +13,37 @@ public class Level2_Land {
 
     public static int solution(int[][] land) {
         int answer = 0;
+        int[][] dp = new int [land.length+1][land[0].length+1];
+
 
         for (int i=0; i<4; i++) {
-            int tmp = land[0][i] + landing(land, 1, i);
+            int tmp = land[0][i] + landing(land, 1, i, dp);
             answer = Math.max(answer, tmp);
         }
         return answer;
     }
 
-    public static int landing(int[][] land, int i, int j) {
-        if (i == land.length)
+    public static int landing(int[][] land, int i, int lastj, int[][] dp) {
+        if (i == land.length) {
+            dp[i-1][lastj] = land[i-1][lastj];
             return 0;
+        }
         else {
             int max = 0;
-            for (int x=0; x<4; x++) {
-                if (x == j)
+            for (int curj=0; curj<4; curj++) {
+                if (curj == lastj)
                     continue;
 
-                int tmp = landing(land, i+1, x);
+                int tmp = 0;
+                if (dp[i][curj] != 0)
+                    tmp = dp[i][curj];
+                else
+                    tmp = land[i][curj] + landing(land, i+1, curj, dp);
+
                 max = Math.max(max, tmp);
             }
-            int sum = max + land[i][j];
-            return sum;
+            dp[i-1][lastj] = max + land[i-1][lastj];
+            return max;
         }
 
     }

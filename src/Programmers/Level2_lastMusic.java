d5f59238963a0ee2bae61f38784efdd7c71c8860
leebongho <bh9204@naver.com>
2020-03-09 22:11:04 +0900

    카카오 코딩테스트 레벨2 [방금 그곡] 문제 풀이 완료.

--- src/Programmers/Level2_lastMusic.java
+++ src/Programmers/Level2_lastMusic.java
@@ -7,61 +7,69 @@ public class Level2_lastMusic {
     public static void main(String[] args) {
         String m = "ABC";
         String[] musicinfos = {
-                "12:00","12:14","HELLO","C#DEFGAB",
-                "13:00","13:05","WORLD","ABCDEF"
+                "12:00,12:14,HELLO,C#DEFGAB",
+                "13:00,13:05,WORLD,ABCDEF"
         };
 
         System.out.println(solution(m, musicinfos));
 
     }
 
-    public static String solution(String m, String[] musicinfos) {
+    public  static String solution(String m, String[] musicinfos) {
 
         int maxPlayTime = 0;
         String targetTitle = "";
-        for (int i=0; i<musicinfos.length; i+=4) {
 
-            int playTime = totalPlayTime(musicinfos[i], musicinfos[i+1]);
-            String title = musicinfos[i+2];
-            List<String> totalMusicMelody = toList(musicinfos[i+3]);
 
-            List<String> listenedMelody = toList(m);
-            List<String> realMelody = realPlayMelody(playTime, totalMusicMelody);
+        for (String info : musicinfos) {
+            String[] musicInfos = info.split(",");
 
-            if (listenedMelody.containsAll(realMelody) || realMelody.containsAll(listenedMelody)) {
-                if (maxPlayTime < playTime) {
+            for (int i=0; i<musicInfos.length; i+=4) {
+
+                int playTime = totalPlayTime(musicInfos[i], musicInfos[i + 1]);
+                String title = musicInfos[i + 2];
+                String totalMusicMelody = parsing(musicInfos[i + 3]);
+
+                String listenedMelody = parsing(m);
+                String realMelody = realPlayMelody(playTime, totalMusicMelody);
+
+                if (isMatched(listenedMelody, realMelody) && maxPlayTime < playTime) {
                     maxPlayTime = playTime;
                     targetTitle = title;
                 }
             }
-
         }
 
-        return targetTitle;
+        return "".equals(targetTitle) ? "(None)" : targetTitle;
     }
 
-    private static int totalPlayTime(String startTime, String endTime) {
+    private  static int totalPlayTime(String startTime, String endTime) {
         int totalHours = Integer.parseInt(endTime.substring(0, 2)) - Integer.parseInt(startTime.substring(0, 2));
         int totalMinutes = Integer.parseInt(endTime.substring(3)) - Integer.parseInt(startTime.substring(3));
 
         return (totalHours*60) + totalMinutes;
     }
 
-    private static List<String> realPlayMelody(int realPlayTime, List<String> totalMusicMelody) {
-        if (realPlayTime > totalMusicMelody.size()) {
-            int extraTime = realPlayTime - totalMusicMelody.size();
-            totalMusicMelody.addAll(realPlayMelody(extraTime, totalMusicMelody));
-            return totalMusicMelody;
+    private  static String realPlayMelody(int realPlayTime, String totalMusicMelody) {
+        if (realPlayTime > totalMusicMelody.length()) {
+            int extraTime = realPlayTime - totalMusicMelody.length();
+            return totalMusicMelody + (realPlayMelody(extraTime, totalMusicMelody));
         } else {
-            List<String> extraMelody = new ArrayList<>();
-            for (int i=0; i<realPlayTime; i++)
-                extraMelody.add(totalMusicMelody.get(i));
-
-            return extraMelody;
+            return totalMusicMelody.substring(0, realPlayTime);
         }
     }
 
-    private static List<String> toList(String totalMusicMelody) {
+    private  static boolean isMatched(String listenedMelody, String realMelody) {
+        return realMelody.indexOf(listenedMelody) > -1 ? true : false;
+    }
+
+    private static String parsing(String totalMusicMelody) {
+        totalMusicMelody = totalMusicMelody.replaceAll("A#", "H")
+                .replaceAll("C#", "I")
+                .replaceAll("D#", "J")
+                .replaceAll("F#", "K")
+                .replaceAll("G#", "L");
+
         List<String> list = new ArrayList<>();
         for (int i=0; i<totalMusicMelody.length(); i++) {
             if (i < totalMusicMelody.length()-1 && totalMusicMelody.charAt(i+1) == '#') {
@@ -72,7 +80,7 @@ public class Level2_lastMusic {
             }
         }
 
-        return list;
+        return totalMusicMelody;
     }
 
 }

f5f567b34928711ce902cc2c99521d4f3c49e6e2
leebongho <bh9204@naver.com>
2020-03-12 07:03:46 +0900

    카카오 코딩테스트 레벨2 [N진수] 문제 풀이 완료.

--- src/Programmers/Level2_KAKAO_Njinsu.java
+++ src/Programmers/Level2_KAKAO_Njinsu.java
@@ -1,4 +1,47 @@
 package Programmers;
 
 public class Level2_KAKAO_Njinsu {
+    public static void main(String[] args) {
+        System.out.println(solution(16, 16, 2, 2));
+
+    }
+
+    public static String solution(int n, int t, int m, int p) {
+
+        StringBuilder totalCode = new StringBuilder();
+        StringBuilder tubeCode = new StringBuilder();
+
+        int i = 0;
+        while (totalCode.length()/m < t) {
+            totalCode.append(convertDecimalToJinsu(n, i));
+            i += 1;
+        }
+
+        int tubeIndex = p - 1;
+        while (tubeCode.length() != t) {
+            tubeCode.append(totalCode.substring(tubeIndex, tubeIndex+1));
+            tubeIndex += m;
+        }
+
+        return tubeCode.toString();
+
+
+    }
+
+    private static String convertDecimalToJinsu(int jinsu, int originDecimal) {
+        String[] jinsuCode = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+        StringBuilder sb = new StringBuilder();
+
+        if (originDecimal == 0)
+            return "0";
+        else {
+            while (originDecimal > 0) {
+
+                sb.insert(0, jinsuCode[originDecimal % jinsu]);
+                originDecimal /= jinsu;
+            }
+        }
+
+        return sb.toString();
+    }
 }

02fa6401a6a64efff12d072f0627c63e2a282d5a
leebongho <bh9204@naver.com>
2020-05-06 07:28:25 +0900

    NumberBaseBall 두 번째 풀이중

--- src/Programmers/rePlay/Leve2_RamenFactory.java
+++ src/Programmers/rePlay/Leve2_RamenFactory.java
@@ -5,35 +5,60 @@ import java.util.*;
 public class Leve2_RamenFactory {
     public static void main(String[] args) {
         int stock = 4;
-        int[] dates = {4, 10, 30};
-        int[] supplies = {20, 5, 10};
-        int k = 30;
+        int[] dates = {1, 2, 3, 5};
+        int[] supplies = {10, 30, 20, 40};
+        int k = 100;
+//
+//        입력값 〉 4, [4, 10, 15], [20, 5, 10], 30
+//        기댓값 〉 2
+//
+//        입력값 〉 10, [5, 10], [1, 100], 110
+//        기댓값 〉 1
+//
+//        입력값 〉 4, [1, 2, 3, 4], [10, 40, 20, 30], 100
+//        기댓값 〉 4
+//
+//        입력값 〉 2, [1], [10], 10
+//        기댓값 〉 1
         System.out.println(solution(stock, dates, supplies, k));
     }
 
     public static int solution(int stock, int[] dates, int[] supplies, int k) {
         int answer = 0;
-        int lastDate = 0;
-        List<SupplyInfo> prQueue = new ArrayList<>();
-
-
+        //List<SupplyInfo> prQueue = new ArrayList<>();
+        PriorityQueue<SupplyInfo> prQueue = new PriorityQueue<>(new Comparator<SupplyInfo>() {
+            @Override
+            public int compare(SupplyInfo o1, SupplyInfo o2) {
+                if (o1.getSupply() > o2.getSupply())
+                    return -1;
+                else if (o1.getSupply() < o2.getSupply())
+                    return 1;
+                else {
+                    if (o1.getDate() > o2.getDate())
+                        return -1;
+                    else
+                        return 0;
+                }
+            }
+        });
         for (int i=0; i<dates.length; i++)
-            prQueue.add(new SupplyInfo(dates[i], supplies[i]));
+            prQueue.offer(new SupplyInfo(dates[i], supplies[i]));
 
-        Collections.sort(prQueue);
+        //Collections.sort(prQueue);
         System.out.println(prQueue);
 
         while (prQueue.size() > 0 && stock < k) {
-            SupplyInfo prSupplyInfo = prQueue.remove(0);
+            SupplyInfo prSupplyInfo = prQueue.poll();
             List<SupplyInfo> tmpList = new ArrayList<>();
 
             while (stock < prSupplyInfo.getDate()) {
                 tmpList.add(prSupplyInfo);
-                prSupplyInfo = prQueue.remove(0);
+                prSupplyInfo = prQueue.poll();
             }
 
             stock += prSupplyInfo.getSupply();
-            prQueue.addAll(tmpList);
+            System.out.println(prSupplyInfo);
+            for (SupplyInfo tmp : tmpList) prQueue.offer(tmp);
 
             answer += 1;
         }
--- src/Programmers/rePlay/Level2_RamenFactory2.java
+++ src/Programmers/rePlay/Level2_RamenFactory2.java
@@ -1,17 +1,29 @@
 package Programmers.rePlay;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Level2_RamenFactory2 {
     public static void main(String[] args) {
+
+
+//        입력값 〉 4, [4, 10, 15], [20, 5, 10], 30
+//        기댓값 〉 2
+//
+//        입력값 〉 10, [5, 10], [1, 100], 110
+//        기댓값 〉 1
+//
+//        입력값 〉 4, [1, 2, 3, 4], [10, 40, 20, 30], 100
+//        기댓값 〉 4
+//
+//        입력값 〉 2, [1], [10], 10
+//        기댓값 〉 1
+
+
         int stock = 4;
-        int[] dates =    {4, 10, 15, 20, 40, 50};
-        int[] supplies = {20, 5, 10, 7, 28, 199};
-        int k = 30;
+        int[] dates =    {1, 2, 3, 5 };
+        int[] supplies = {10, 0, 20, 30};
+        int k = 100;
         System.out.println(solution(stock, dates, supplies, k));
     }
 
@@ -20,15 +32,16 @@ public class Level2_RamenFactory2 {
 
         int lastDate = 0;
         List<Integer> dateList = Arrays.stream(dates).boxed().collect(Collectors.toList());
+        List<Integer> tmpDataList = new ArrayList<>(dateList);
         List<Integer> supplyList = Arrays.stream(supplies).boxed().collect(Collectors.toList());
 
 
         Collections.sort(dateList, new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if (supplies[dateList.indexOf(o1)] > supplies[dateList.indexOf(o2)])
+                if (supplyList.get(tmpDataList.indexOf(o1)) > supplyList.get(tmpDataList.indexOf(o2)))
                     return -1;
-                else if (supplies[dateList.indexOf(o1)] < supplies[dateList.indexOf(o2)])
+                else if (supplyList.get(tmpDataList.indexOf(o1)) < supplyList.get(tmpDataList.indexOf(o2)))
                     return 1;
                 else
                     return 0;
@@ -36,30 +49,22 @@ public class Level2_RamenFactory2 {
         });
 
         Collections.sort(supplyList, Collections.reverseOrder());
-
-
-        System.out.println(dateList);
-        System.out.println(supplyList);
+//
+//
+//        System.out.println(dateList);
+//        System.out.println(supplyList);
 
         while (dateList.size() > 0 && stock < k) {
             int date = dateList.remove(0);
             int supply = supplyList.remove(0);
-
-            if (date < lastDate)
-                continue;
 
             if (stock < date) {
                 dateList.add(date);
                 supplyList.add(supply);
                 continue;
             }
-
-
             stock += supply;
-            lastDate = date;
             answer += 1;
-
-
         }
 
 

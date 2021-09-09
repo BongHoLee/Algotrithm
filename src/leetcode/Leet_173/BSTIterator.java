f5d2e16e049cf9b50289d1b2416e31477adae78a
leebongho <bh9204@naver.com>
2021-09-09 16:49:56 +0900

    LEET 스레드 이진 탐색 트리 173 문제 풀이

--- src/leetcode/Leet_173/BSTIterator.java
+++ src/leetcode/Leet_173/BSTIterator.java
@@ -5,6 +5,19 @@ import leetcode.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// 트리 중위순회
+// 첫 번째 내가 직접 구현한 코드
+
+/*
+* 중위 순회의 특성상 경로가 (L-V-R)
+* 모든 경로를 우선 (Queue)에 저장먼저 진행
+* */
+
+// Acceptance가 되었지만 효율성 측면에서 좋지 않은 점수를 받음
+// 다른 코드 참고(BSTIterator_better)
+
+
+
 public class BSTIterator {
     private Queue<TreeNode> iteratorQueue = new LinkedList<>();
 
---
+++ src/leetcode/Leet_173/BSTIterator_better.java
@@ -0,0 +1,76 @@
+package leetcode.Leet_173;
+
+import leetcode.TreeNode;
+
+
+/*
+* Threaded Binary tree를 이용한 중위 순회
+* */
+
+public class BSTIterator_better {
+
+    private TreeNode cur;
+
+    public BSTIterator_better(TreeNode root) {
+        this.cur = root;
+        init();
+    }
+
+    private void init() {
+        TreeNode current = cur;
+        TreeNode prev;
+
+        while (current != null) {
+
+            if (current.left != null) {
+                prev = current.left;
+
+                while(prev.right != null && prev.right != current) {
+                    prev = prev.right;
+                }
+
+                if (prev.right == null) {
+                    prev.right = current;
+                    current = current.left;
+                } else {
+                    current = current.right;
+                }
+
+            } else {
+                current = current.right;
+            }
+        }
+
+        while (cur != null && cur.left != null) {
+            cur = cur.left;
+        }
+
+    }
+
+    public int next() {
+        int result = cur.val;
+
+        TreeNode next = cur.right;
+        if (next == null) {
+            cur = null;
+        } else if (next.left == null || next.left.val > cur.val) {
+            cur = next;
+            while(cur.left != null) {
+                cur = cur.left;
+            }
+        } else {
+            cur.right = null;
+            cur = next;
+        }
+
+
+        return result;
+    }
+
+    public boolean hasNext() {
+        return cur != null;
+    }
+
+
+
+}

4f01a10537e5b9cc8bce7f57a46108c4f0a85118
leebongho <bh9204@naver.com>
2021-09-10 13:21:13 +0900

    LEET 트리 pre-order 관련 문제 풀이

---
+++ src/leetcode/Leet_331/Leet_331.java
@@ -0,0 +1,59 @@
+package leetcode.Leet_331;
+
+// 주어진 String이 preorder를 만족하면 true, 아니면 참
+// #은 null node를 의미한다.
+
+// preorder의 규칙을 먼저 알아야 함(ROOT -> LEFT -> RIGHT)
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Leet_331 {
+    public static void main(String[] args) {
+        //String preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#";
+        String preorder = "9,3,#,4,#,#,1,#,#,2,#,6,#,#";
+        System.out.println(isValidSerialization(preorder));
+    }
+
+    public static boolean isValidSerialization(String preorder) {
+        RefInt pos = new RefInt(0);
+        String[] c = preorder.split(",");
+
+        int n = c.length - 1;
+
+        callit(c, pos, n);
+
+        if (pos.idx == n)
+            return true;
+        return false;
+    }
+
+    //
+    public static boolean callit(String[] order, RefInt pos, int n) {
+        //"9,3,#,4,#,#,1,#,#,2,#,6,#,#";
+        if (order[pos.idx].equals("#"))
+            return true;
+
+        // left subtree is valid
+        pos.idx += 1;
+        if(pos.idx > n || !callit(order, pos, n))
+            return false;
+
+        // right subtree
+        pos.idx += 1;
+        if (pos.idx > n || !callit(order, pos, n))
+            return false;
+
+        return true;
+    }
+}
+
+class RefInt {
+    public Integer idx;
+
+    RefInt(Integer idx) {
+        this.idx = idx;
+    }
+}

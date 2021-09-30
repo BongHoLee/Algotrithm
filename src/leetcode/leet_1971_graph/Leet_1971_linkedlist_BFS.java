bfae61c5662eb068a4e126f0ed868ef4001a4e82
leebongho <bh9204@naver.com>
2021-09-30 16:52:47 +0900

    Leet 1971 그래프 easy 풀이

---
+++ src/leetcode/leet_1971_graph/Leet_1971_linkedlist_BFS.java
@@ -0,0 +1,71 @@
+package leetcode.leet_1971_graph;
+
+import java.util.*;
+
+public class Leet_1971_linkedlist_BFS {
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] edges = {
+                {0, 1},
+                {0, 2},
+                {3, 5},
+                {5, 4},
+                {4, 3}
+        };
+        int start = 0;
+        int end = 5;
+
+        System.out.println(validPath(n, edges, start, end));
+    }
+
+    public static boolean validPath(int n, int[][] edges, int start, int end) {
+        if (n <= 0 || start == end) {
+            return true;
+        }
+        Map<Integer, List<Integer>> graph = makeGraph(edges);
+        boolean[] visit = new boolean[n];
+
+        Queue<Integer> bfsQ = new LinkedList<>();
+        bfsQ.add(start);
+        visit[start] = true;
+
+        while(!bfsQ.isEmpty()) {
+            Integer node = bfsQ.remove();
+
+            List<Integer> adjacent = graph.get(node);
+            for (Integer each : adjacent) {
+                if (!visit[each]) {
+                    if (each == end) {
+                        return true;
+                    } else {
+                        visit[each] = true;
+                        bfsQ.add(each);
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
+    private static Map<Integer, List<Integer>> makeGraph(int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] edge : edges) {
+            if (!graph.containsKey(edge[0])) {
+                graph.put(edge[0], new ArrayList<>());
+            }
+
+            if (!graph.containsKey(edge[1])) {
+                graph.put(edge[1], new ArrayList<>());
+            }
+
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        return graph;
+    }
+}
---
+++ src/leetcode/leet_1971_graph/Leet_1971_matrix_BFS.java
@@ -0,0 +1,65 @@
+package leetcode.leet_1971_graph;
+
+// vertices : 정점(노드)
+// vertex : 인접 정점(연결된 정점)
+// edges : 간선
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Leet_1971_matrix_BFS {
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] edges = {
+                {0, 1},
+                {0, 2},
+                {3, 5},
+                {5, 4},
+                {4, 3}
+        };
+        int start = 0;
+        int end = 5;
+
+        System.out.println(validPath(n, edges, start, end));
+
+    }
+
+    public static boolean validPath(int n, int[][] edges, int start, int end) {
+        if (n == 1 && start == end) {
+            return true;
+        }
+        int[][] matrix = makeMatrix(n, edges);
+
+        Queue<Integer> bfsQ = new LinkedList<>();
+        bfsQ.add(start);
+
+        while (!bfsQ.isEmpty()) {
+            int node = bfsQ.remove();
+            for (int x = 0; x < matrix[node].length; x++) {
+                if (matrix[node][x] != 0) {
+                    if (x == end) {
+                        return true;
+                    } else {
+                        matrix[node][x] = 0;
+                        matrix[x][node] = 0;
+                        bfsQ.add(x);
+                    }
+
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static int[][] makeMatrix(int n, int[][] edges) {
+        int[][] matrix = new int[n][n];
+
+        for (int[] edge : edges) {
+            matrix[edge[0]][edge[1]] = 1;
+            matrix[edge[1]][edge[0]] = 1;
+        }
+
+        return matrix;
+    }
+}

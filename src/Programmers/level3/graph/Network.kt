b8ec2f7687ce5802dba0e45e7ca584a73244ba04
leebongho <bh9204@naver.com>
2021-10-21 09:32:38 +0900

    프로그래머스 레벨3 그래프 탐색(Network) 문제 풀이 완료

---
+++ src/Programmers/level3/graph/Network.kt
@@ -0,0 +1,48 @@
+package Programmers.level3.graph
+
+class Network {
+    fun solution(n: Int, computers: Array<IntArray>): Int {
+        var networkCount = 0
+        val visited = BooleanArray(n)
+        val graph = hashMapOf<Int, MutableList<Int>>()
+        computers.forEachIndexed {y, connected ->
+            connected.forEachIndexed { x, computer ->
+                if (computer == 1) {
+                    graph.getOrPut(y) { mutableListOf()}.add(x)
+                }
+            }
+        }
+
+        for (entry in graph) {
+            val curComputer = entry.key
+            if (!visited[curComputer])  {
+                networkCount++
+                dfs(curComputer, visited, graph)
+            }
+        }
+
+        return networkCount
+    }
+
+    private fun dfs(node: Int, visited: BooleanArray, graph: MutableMap<Int, MutableList<Int>>) {
+        if (!visited[node]) {
+            visited[node] = true
+
+            for (connected in graph.getOrDefault(node, mutableListOf())) {
+                dfs(connected, visited, graph)
+            }
+
+        }
+    }
+}
+
+fun main() {
+    val n = 3
+    val computers = arrayOf(
+        intArrayOf(1, 1, 0),
+        intArrayOf(1, 1, 1),
+        intArrayOf(0, 1, 1)
+    )
+
+    println(Network().solution(n, computers))
+}

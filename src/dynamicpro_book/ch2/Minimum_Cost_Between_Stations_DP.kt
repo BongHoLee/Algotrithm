57b87c960d7faf7a39fd6e816ff1da07e1c27a43
leebongho <bh9204@naver.com>
2021-12-20 16:30:28 +0900

    역사이거리 다이내믹프로그래믹(상향식) 고생

---
+++ src/dynamicpro_book/ch2/Minimum_Cost_Between_Stations_DP.kt
@@ -0,0 +1,44 @@
+package dynamicpro_book.ch2
+
+import kotlin.math.sign
+
+// 꽤나 헤맸지?
+// 생각해보면 ㅈㄴ 쉬운거네
+
+class Minimum_Cost_Between_Stations_DP {
+
+    fun minimumCost(stations: Array<IntArray>) : Int {
+        val minCost = Array(stations.size) {IntArray(stations[0].size)}
+
+        for (y in stations.indices) {
+            var sumMin = Int.MAX_VALUE
+            for (x in y until stations[y].size) {
+                if (y==0) {
+                    minCost[y][x] = stations[y][x]
+                } else {
+                    if (y == x) {
+                        minCost[y][x] = minCost[y-1][x]
+                    } else {
+                        minCost[y][x] = Math.min(minCost[y-1][x], stations[y][x] + minCost[y-1][x-1])
+                    }
+                }
+            }
+        }
+
+        val j = stations.size - 1
+        val i = stations[0].size - 1
+        return minCost[j][i]
+    }
+}
+
+
+fun main() {
+    val stations = arrayOf(
+        intArrayOf(0, 10, 75, 140),
+        intArrayOf(-1, 0, 35, 130),
+        intArrayOf(-1, -1, 0, 80),
+        intArrayOf(-1, -1, -1, 0)
+    )
+
+    println(Minimum_Cost_Between_Stations_DP().minimumCost(stations))
+}

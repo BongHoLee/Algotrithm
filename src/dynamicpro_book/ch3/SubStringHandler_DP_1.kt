98b8f820477c6c6d3077c5e88775e98990f3412d
leebongho <bh9204@naver.com>
2021-12-22 00:49:44 +0900

    가장 긴 substring 길이 - DP 답은 맞았는데 식이 너무.. 다시봐도 이해 안되고 헷갈리게 세움

--- src/dynamicpro_book/ch3/SubStringHandler_DP_1.kt
+++ src/dynamicpro_book/ch3/SubStringHandler_DP_1.kt
@@ -9,17 +9,24 @@ class SubStringHandler_DP_1 {
         str.forEachIndexed {index, each -> arr[index][index] = Character.getNumericValue(each)}
         var substr = ""
 
-        for (y in 1 until str.length) {
+        // 왜맞음... 어케 맞은거임..
+        for (y in 1 until str.length-1) {
             for (x in 0 until str.length - y) {
+                // k가 중요 변수 ? k와 관련한 식으로 다시 세워보자.
+
                 // arr(from, to) -> 부분문자열 from ~ to 까지의 합
-                // k가 중요 변수 ?
                 // arr[0][1], arr[1][2] ... => arr[x][x+y]
                 // 대각선 채우기
                 val left = arr[x][x+y-1]
                 val right = arr[x+y][x+y]
                 arr[x][x+y] = left + right
 
-                if ()
+                if ((x+x+y+1)%2 == 0) {
+                    val splitIdx = (x+x+y+1)/2
+                    if (arr[x][splitIdx-1] == arr[splitIdx][x+y] && substr.length < str.substring(x, x+y+1).length) {
+                        substr = str.substring(x, x+y+1)
+                    }
+                }
             }
         }
 
@@ -29,6 +36,6 @@ class SubStringHandler_DP_1 {
 }
 
 fun main() {
-    val str = "9430723"
+    val str = "9430723129"
     println(SubStringHandler_DP_1().handle(str))
 }
